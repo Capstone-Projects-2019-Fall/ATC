9d585a25b707e5c6f16bc641690193599dbbc38e
Steven Liu <tuf56325@temple.edu>
2019-11-03 09:42:27 -0500

    added ease of connectina to webapp

--- tornado/home.py
+++ tornado/home.py
@@ -1,12 +1,12 @@
 import tornado.ioloop
 import tornado.web
-
+import socket 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
         items = ["Item 1", "Item 2", "Item 3"]
-        self.render("template.html", title="My title", items=items)
+        self.render("index.html", title="My title", items=items)
 
 def make_app():
     return tornado.web.Application([
@@ -14,6 +14,24 @@ def make_app():
     ])
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8008)
-    tornado.ioloop.IOLoop.current().start()
+    try: 
+        host_name = socket.gethostname() 
+        host_ip = socket.gethostbyname(host_name) 
+        print("Hostname :  ",host_name) 
+        print("IP : ",host_ip) 
+    except: 
+        print("Unable to get Hostname and IP")
+
+    try:
+        port = 8009
+        app = make_app()
+        app.listen(port)
+        print("connect to http://" + host_ip + ":" + str(port))
+        tornado.ioloop.IOLoop.current().start()
+    # signal : CTRL + BREAK on windows or CTRL + C on linux
+    except KeyboardInterrupt:
+        print()
+        print("stoppping loop and closed port")
+        tornado.ioloop.IOLoop.current().stop()
+        
+    
