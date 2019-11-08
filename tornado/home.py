af8d2c58062154a86fc3ec56219907f6015252b0
Steven Liu <tuf56325@temple.edu>
2019-11-08 17:11:51 -0500

    added webcam code

--- tornado/home.py
+++ tornado/home.py
@@ -111,17 +111,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
-    output = StreamingOutput()
-    #Uncomment the next line to change your Pi's Camera rotation (in degrees)
-    #camera.rotation = 90
-    camera.start_recording(output, format='mjpeg')
-    try:
-        address = ('', 8000)
-        server = StreamingServer(address, StreamingHandler)
-        server.serve_forever()
-    finally:
-        camera.stop_recording()
+
 
 
 if __name__ == "__main__":
@@ -132,6 +122,18 @@ if __name__ == "__main__":
         print("IP : ",host_ip) 
     except: 
         print("Unable to get Hostname and IP")
+
+    with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+        output = StreamingOutput()
+    #Uncomment the next line to change your Pi's Camera rotation (in degrees)
+        camera.rotation = 180
+        camera.start_recording(output, format='mjpeg')
+    try:
+        address = ('', 8000)
+        server = StreamingServer(address, StreamingHandler)
+        server.serve_forever()
+    finally:
+        camera.stop_recording()
 
     try:
         port = 8000
@@ -145,5 +147,7 @@ if __name__ == "__main__":
         print()
         print("stopping loop and closed port")
         tornado.ioloop.IOLoop.current().stop()
+    
+    
         
     
